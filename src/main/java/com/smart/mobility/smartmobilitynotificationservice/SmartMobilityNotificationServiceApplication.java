2fa3509f2ba0ad8e086af3541a30ecef0e94fcda
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-02-23 11:02:58 +0000

    Initial commit - Notification Service

---
+++ src/main/java/com/smart/mobility/smartmobilitynotificationservice/SmartMobilityNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smart.mobility.smartmobilitynotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMobilityNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMobilityNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smart/mobility/smartmobilitynotificationservice/SmartMobilityNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.mobility.smartmobilitynotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMobilityNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
